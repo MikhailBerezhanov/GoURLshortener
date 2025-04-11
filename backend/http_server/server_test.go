03880540ff24579a49a18ca6982cb973ef07c1a8
Mikhail Berezhanov <berezhanov.m@gmail.com>
2025-04-11 19:15:21 +0300

    backend/server_test: getHandler tests added

--- backend/http_server/server_test.go
+++ backend/http_server/server_test.go
@@ -3,6 +3,8 @@ package http_server
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -10,19 +12,42 @@ import (
 	"url_shortener/url"
 )
 
+type FailDBMock struct{}
+
+func (f FailDBMock) InsertRecord(*url.Record) error { return fmt.Errorf("mock insert error") }
+func (f FailDBMock) SelectRecord(string) (url.Record, error) {
+	return url.Record{}, fmt.Errorf("mock insert error")
+}
+
 func createMockRequest(method, url, body string) (req *http.Request, w *httptest.ResponseRecorder) {
-	req = httptest.NewRequest(method, url, bytes.NewBufferString(body))
+	var bodyReader io.Reader
+	if len(body) > 0 {
+		bodyReader = bytes.NewBufferString(body)
+	}
+
+	req = httptest.NewRequest(method, url, bodyReader)
 	w = httptest.NewRecorder()
 	return
 }
+
+func equal(r1, r2 *url.Record) bool {
+	return r1.Id == r2.Id && r1.ShortCode == r2.ShortCode && r1.URL == r2.URL
+}
+
+func createHandlerUnderTest(h func(w http.ResponseWriter, request *http.Request)) (handler http.HandlerFunc, mockDB *datastore.MemDB) {
+	mockDB = datastore.NewMemDB()
+	handler = withRecoveryAndContext(h, mockDB)
+	return
+}
+
+// POST /shorten tests
 
 func TestPostHandlerShouldReturn400OnBadRequest(t *testing.T) {
 	body := `{"invalidFieldName": "http://test/url.org"}`
 	req, w := createMockRequest(http.MethodPost, "/shorten", body)
 	req.Header.Set("Content-Type", "application/json")
 
-	mockDB := datastore.NewMemDB()
-	handler := withRecoveryAndContext(postHandler, mockDB)
+	handler, _ := createHandlerUnderTest(postHandler)
 	handler(w, req)
 
 	resp := w.Result()
@@ -38,8 +63,7 @@ func TestPostHandlerShouldReturn415OnUnsupportedContentType(t *testing.T) {
 	req, w := createMockRequest(http.MethodPost, "/shorten", body)
 	req.Header.Set("Content-Type", "text/plain")
 
-	mockDB := datastore.NewMemDB()
-	handler := withRecoveryAndContext(postHandler, mockDB)
+	handler, _ := createHandlerUnderTest(postHandler)
 	handler(w, req)
 
 	resp := w.Result()
@@ -50,13 +74,29 @@ func TestPostHandlerShouldReturn415OnUnsupportedContentType(t *testing.T) {
 	}
 }
 
+func TestPostHandlerShouldReturn500OnFailedInsertToDB(t *testing.T) {
+	body := `{"url": "http://test/url.org"}`
+	req, w := createMockRequest(http.MethodPost, "/shorten", body)
+	req.Header.Set("Content-Type", "application/json")
+
+	var mockDB FailDBMock
+	handler := withRecoveryAndContext(postHandler, &mockDB)
+	handler(w, req)
+
+	resp := w.Result()
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusInternalServerError {
+		t.Errorf("expected status 500 InternalServerError, got %v", resp.Status)
+	}
+}
+
 func TestPostHandlerShouldCreateNewRecord(t *testing.T) {
 	body := `{"url": "http://test/url.org"}`
 	req, w := createMockRequest(http.MethodPost, "/shorten", body)
 	req.Header.Set("Content-Type", "application/json")
 
-	mockDB := datastore.NewMemDB()
-	handler := withRecoveryAndContext(postHandler, mockDB)
+	handler, mockDB := createHandlerUnderTest(postHandler)
 	handler(w, req)
 
 	resp := w.Result()
@@ -76,7 +116,81 @@ func TestPostHandlerShouldCreateNewRecord(t *testing.T) {
 		t.Errorf("expected record %s was inserted to db, actual select failed with err: %v", &rec, err)
 	}
 
-	// if rec != recFromDB {
-	// 	t.Errorf("expected record selected from db and received from response will be eqaul, actual rec: %s, recFromDB: %s", &rec, &recFromDB)
-	// }
+	if !equal(&rec, &recFromDB) {
+		t.Errorf("expected record selected from db and received from response will be eqaul, actual rec: %s, recFromDB: %s", &rec, &recFromDB)
+	}
+}
+
+// `GET` /shorten/<shortCode> tests
+
+func TestGetHandlerShouldReturn400OnBadRequest(t *testing.T) {
+	req, w := createMockRequest(http.MethodGet, "/shorten/", "")
+
+	handler, _ := createHandlerUnderTest(getHandler)
+	handler(w, req)
+
+	resp := w.Result()
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusBadRequest {
+		t.Errorf("expected status 400 BadRequest, got %v", resp.Status)
+	}
+}
+
+func TestGetHandlerShouldReturn404OnNotFoundShortCode(t *testing.T) {
+	req, w := createMockRequest(http.MethodGet, "/shorten/abc123", "")
+
+	handler, _ := createHandlerUnderTest(getHandler)
+	handler(w, req)
+
+	resp := w.Result()
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusNotFound {
+		t.Errorf("expected status 404 NotFound, got %v", resp.Status)
+	}
+}
+
+func TestGetHandlerShouldReturn500OnFailedSelectFromDB(t *testing.T) {
+	req, w := createMockRequest(http.MethodGet, "/shorten/abc123", "")
+
+	var mockDB FailDBMock
+	handler := withRecoveryAndContext(getHandler, &mockDB)
+	handler(w, req)
+
+	resp := w.Result()
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusInternalServerError {
+		t.Errorf("expected status 500 InternalServerError, got %v", resp.Status)
+	}
+}
+
+func TestGetHandlerShouldReturnRecordByShortCode(t *testing.T) {
+	mockDB := datastore.NewMemDB()
+	testRecord := url.NewRecord("test/url")
+	if err := mockDB.InsertRecord(testRecord); err != nil {
+		t.Errorf("expected mockDB will InsertRecord, actual failed with error %v", err)
+	}
+
+	req, w := createMockRequest(http.MethodGet, "/shorten/"+testRecord.ShortCode, "")
+
+	handler := withRecoveryAndContext(getHandler, mockDB)
+	handler(w, req)
+
+	resp := w.Result()
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("expected status 200 OK, got %v", resp.Status)
+	}
+
+	var receivedRecord url.Record
+	if err := json.NewDecoder(resp.Body).Decode(&receivedRecord); err != nil {
+		t.Errorf("expected record decoded from json response body, actual failed with err: %v", err)
+	}
+
+	if !equal(testRecord, &receivedRecord) {
+		t.Errorf("expected testRecord and received from response will be eqaul, actual testRecord: %s, receivedRecord: %s", testRecord, &receivedRecord)
+	}
 }
